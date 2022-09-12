6687074f5c0e3e412d777d918e1f3fb7725878e0
BruceRMack <69060045+BruceRMack@users.noreply.github.com>
2022-09-13 01:01:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace onenote_to_obsidian_md_path_fix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
