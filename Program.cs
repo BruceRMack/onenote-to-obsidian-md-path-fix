0fc2488a1cc5875930ee7ef71a0a9a2b3a912c7d
BruceRMack <69060045+BruceRMack@users.noreply.github.com>
2022-09-24 22:38:39 +0200

    Extract method for media paths. Removed C: hardcoding

--- Program.cs
+++ Program.cs
@@ -4,44 +4,33 @@
     {
         static void Main(string[] args)
         {
-            String removedcurlies = removeImageWidths(File.ReadAllText(@"C:\Temp\notes\MMSA\Finance\Loading-Allocations.md"));
-            return;
-            string path = @"C:\temp\notes\MMSA";
+            string path = args[0];
             string[] files = Directory.GetFiles(path, "*.md", SearchOption.AllDirectories);
 
             foreach (string file in files)
             {
-                String old_path = file.Substring(0, file.LastIndexOf('\\')) + "/media/";
-                old_path = old_path.Replace("C:", "c:");
-                old_path = Char.ToLower(old_path[0]) + old_path.Substring(1);
-                //Console.WriteLine(old_path);
 
-                    string fileContent = File.ReadAllText(file);
-                if (fileContent.Contains(old_path))
-                    Console.WriteLine("url match found in " + file);
-                string newContent = fileContent.Replace(old_path, "media/");
-                //Console.WriteLine(file.Substring(0, file.LastIndexOf('\\'))+"/media/");
+                string fileContent = File.ReadAllText(file);
+                String newContent = fixMediaPaths(file, fileContent);
                 //replace width in inches
                 //eg{width="7.25in" height="5.28125in"}
-                //if (!newContent.Contains({width"))
                 newContent = removeImageWidths(newContent);
+                
                 if (!newContent.Equals(fileContent))
                 {
-                    Console.WriteLine("path found " + file);
                     File.WriteAllText(file, newContent);
                 }
-                /*
-                if (file.Substring(0, file.LastIndexOf('\\')).Length > 0)
-                {
-                    Console.WriteLine("path found " + file + ":" + file.Substring(0, file.LastIndexOf('\\') + 6));
-                    return;
-                }
-                */
-//                    if (newContent.Contains("<p>"))
-//                    Console.WriteLine("Page is html "+file);
-                //Console.WriteLine(newContent);
-                //return;
+
             }
+        }
+
+        private static string fixMediaPaths(string file, string fileContent)
+        {
+            String old_path = file.Substring(0, file.LastIndexOf('\\')) + "/media/";
+            //lower first character
+            old_path = old_path[0].ToString().ToLower() + old_path.Substring(1);
+            old_path = Char.ToLower(old_path[0]) + old_path.Substring(1);
+            return fileContent.Replace(old_path, "media/");
         }
 
         private static string removeImageWidths(string newContent)
@@ -51,7 +40,6 @@
             {
                 int startPosition = newStr.IndexOf("){width")+1;
                 int endPosition = newStr.IndexOf("\"}")+2;
-                Console.WriteLine("Chop out from "+startPosition+" to "+endPosition+":"+newStr.Substring(startPosition,endPosition-startPosition));  
                 String beforeStart = newStr.Substring(1, startPosition-1);
                 String afterEnd = newStr.Substring(endPosition);
                 newStr = beforeStart + afterEnd;
